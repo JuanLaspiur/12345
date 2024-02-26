7e126340d887f78e81e819f32054d490f453c59d
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-02-26 16:47:22 -0300

    nina

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api': {
-      target: 'https://coraqr-back2.onrender.com/api/', 
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''), 
-    }
-  } 
 })
